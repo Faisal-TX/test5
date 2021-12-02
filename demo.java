b4bffc431a6adbf3b245a98e06917838d041b45d
Faisal <faisalmohammad875@gmail.com>
2021-12-02 22:35:01 +0530

    first commit

---
+++ demo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class demo
+{
+public static void main(String[] args)
+{
+int age;
+System.out.println("write age");
+Scanner sc = new Scanner(System.in);
+age = sc.nextInt();
+
+if(age>10)
+{
+System.out.println("hi");
+}
+else
+{
+System.out.println("bye");
+}
+}
+}
